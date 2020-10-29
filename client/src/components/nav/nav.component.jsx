7d4e87ccf86e9a6e4e1a13ef1758d21dbc7da3df
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-28 20:01:00 -0400

    add input components to the signup form

--- client/src/components/nav/nav.component.jsx
+++ client/src/components/nav/nav.component.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './nav.styles.scss'
 
 const Nav = () => (
-    <nav>
-        <p>hello</p>
-    </nav>
+    <div className="header">
+        <div className="siteActions">
+            <Link to='/' className='option logo'>Reno</Link>
+            <Link to='/services' className='option logo'>Services</Link>
+            <Link to='/tools' className='option logo'>Tools</Link>
+
+        </div>
+        <div className="userOptions">
+            <Link to='/signup' className="option">Sign Up</Link>
+            <div className="option">Sign Out</div>
+        </div>
+    </div>
 )
 
 export default Nav
--- client/src/pages/sign-up/sign-up.component.jsx
+++ client/src/pages/sign-up/sign-up.component.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import TextField from '@material-ui/core/TextField';
+import FormGroup from '@material-ui/core/FormGroup';
+import Button from '@material-ui/core/Button';
 
 import './sign-up.styles.scss';
 
-const SignUp = () => (
-    <h1>SignUp</h1>
-)
+const SignUp = () => {
 
+    const { formData, setFormData } = useState({
+        email: '',
+        firstName: '',
+        lastName: '',
+        userName: '',
+        pasword: '',
+        confirmPassword: ''
+    })
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('yes')
+    }
+
+    return (
+        <div className="sign-up">
+            <h1>SignUp</h1>
+            <form className="sign-up-form" onSubmit={handleSubmit}>
+                <TextField
+                    id="outlined-email-input"
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-first-name-input"
+                    label="First Name"
+                    type="text"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-last-Name-input"
+                    label="Last Name"
+                    type="text"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-username-input"
+                    label="Username"
+                    type="text"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-confirm=password-input"
+                    label="Confirm Password"
+                    type="password"
+                    variant="outlined"
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </div>
+    )
+}
 export default SignUp;
