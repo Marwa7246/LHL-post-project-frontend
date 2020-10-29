5f7c3020eec71dbd36cf97c7fc60ee5b6af0ecbc
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-29 10:32:17 -0400

    make signup handleChange and handleSubmit

--- client/src/pages/sign-up/sign-up.component.jsx
+++ client/src/pages/sign-up/sign-up.component.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import axios from 'axios';
+
 import TextField from '@material-ui/core/TextField';
 import FormGroup from '@material-ui/core/FormGroup';
 import Button from '@material-ui/core/Button';
@@ -8,22 +10,27 @@ import './sign-up.styles.scss';
 
 const SignUp = () => {
 
-    const { formData, setFormData } = useState({
+    const [formData, setFormData] = useState({
         email: '',
         firstName: '',
         lastName: '',
         userName: '',
-        pasword: '',
+        password: '',
         confirmPassword: ''
     })
 
     const handleChange = event => {
-        
+        const { name, value } = event.target;
+        setFormData({...formData, [name]: value })
+        console.log(formData)
     }
 
     const handleSubmit = event => {
-        event.preventDefault();
-        console.log('yes')
+        axios.post('/signup', {
+            formData: formData
+        })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
     }
 
     return (
@@ -32,39 +39,51 @@ const SignUp = () => {
             <form className="sign-up-form" onSubmit={handleSubmit}>
                 <TextField
                     id="outlined-email-input"
+                    name="email"
                     label="Email"
                     type="email"
                     variant="outlined"
+                    onChange={handleChange} 
                 />
                 <TextField
                     id="outlined-first-name-input"
                     label="First Name"
+                    name="firstName"
                     type="text"
                     variant="outlined"
+                    onChange={handleChange} 
                 />
                 <TextField
                     id="outlined-last-Name-input"
                     label="Last Name"
+                    name="lastName"
                     type="text"
                     variant="outlined"
+                    onChange={handleChange} 
                 />
                 <TextField
                     id="outlined-username-input"
                     label="Username"
+                    name="username"
                     type="text"
                     variant="outlined"
+                    onChange={handleChange} 
                 />
                 <TextField
                     id="outlined-password-input"
                     label="Password"
+                    name="password"
                     type="password"
                     variant="outlined"
+                    onChange={handleChange} 
                 />
                 <TextField
                     id="outlined-confirm=password-input"
                     label="Confirm Password"
                     type="password"
+                    name="confirmPassword"
                     variant="outlined"
+                    onChange={handleChange} 
                 />
                 <Button type="submit">Submit</Button>
             </form>
