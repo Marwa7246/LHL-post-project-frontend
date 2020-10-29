400973d948c0fd5e7d2e838b078841c3df3c0f71
Marwa7246 <marwa.ragheb@gmail.com>
2020-10-29 18:57:57 -0400

    Add login component and route

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,16 @@
-exports.signUp = (req, res, next)=> {
+
+signUp = (req, res, next)=> {
     console.log('route hit');
     console.log(req.body);
     res.status(200).json({msg: 'success'})
-}
+};
+
+login = (req, res, next)=> {
+    console.log('route hit-login');
+    console.log(req.body);
+    res.status(200).json({msg: 'success-login'})
+};
+
+
+
+module.exports= {signUp, login}
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/users');
 
 router.post('/api/signup', userController.signUp)
+router.post ('/api/login', userController.login)
 
 module.exports = router
